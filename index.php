49d5757d8df6e55f6002bf679dd8ba2783ac45ef
Mahendra Sahu <Mahendrasahu203@hotmail.com>
2021-01-21 17:20:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<pre>
+
+<html>
+        <body bgcolor=aqua>
+
+                <h1> Node-2 </h1>
+
+        </body>
+</html>
+
+<?php
+
+print(" This web server is operational   !!! \n\n\n\n\n\n\n");
+
+print `ifconfig eth0`;
+
+?>
+
